5a0efc084d2c0dff066d575114c06e8d7986c652
Arthur Melkonyan <arthur@artmelkon.me>
2020-02-29 09:48:06 -0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const feedRoutes = require('./routes/feed');
 const app = express();
 
-app.use('/feed', feedRoutes)
+app.use(bodyParser.json());
+app.use( ( req, res, nex ) => { // set headers
+  res,setHeader('Access-Control_Allow_Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+});
+app.use('/feed', feedRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => ( console.log(`NodeJS Server running on Port: ${PORT}`)))
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,3 +3,14 @@ exports.getPosts = ( req, res, nex ) => {
     posts: [{ title: "First Post", content: "This is the first Post!" }]
   });
 };
+
+exports.createPost = ( req,res, next ) => {
+  // create post in db
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "post created successfully",
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+}
